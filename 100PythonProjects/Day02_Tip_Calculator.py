94df9eb3331a87742b1acfd20ee2cb87114477cd
vishShelke28 <vishalshelke@live.com>
2022-10-23 14:06:53 +0530

    Initial Commit

--- 100PythonProjects/Day02_Tip_Calculator.py
+++ 100PythonProjects/Day02_Tip_Calculator.py
@@ -6,7 +6,7 @@ total_bill = float(input("What was the total bill? INR "))
 tip = int(input("How much percentage you want to give tip? 10, 12 or 15? "))
 people = int(input("Total bill will be divided in how many people? "))
 
-bill_with_tip = total_bill + (total_bill *(tip/100))
+bill_with_tip = total_bill + (total_bill * (tip/100))
 
 # Python round() function float point number from the decimal value to the closest multiple of 10.
 # The int value to the closest multiple of 10 to the power minus ndigits,
