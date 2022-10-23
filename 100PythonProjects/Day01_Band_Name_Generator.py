5f884f8bd721f1d95082719216809e4c8d23cf25
vishShelke28 <vishalshelke@live.com>
2022-10-23 13:07:07 +0530

    Initial Commit

---
+++ 100PythonProjects/Day01_Band_Name_Generator.py
@@ -0,0 +1,14 @@
+# Program to generate band name.
+
+print("Welcome to Band Name Generator Program")
+
+# Enter name of your city and pet.
+# "\n" is used to move cursor to next page during data input. Please note, "\n" is to be used along with the text.
+city = input("What is the name of the City you grown :\n")
+pet = input("What is name of your fav pet :\n")
+
+# Combine city and pet name to generate band name.
+# Band_Name = city + pet
+# print("Band Name :" + Band_Name)
+
+print("Name of the Band is\n" + city + " " + pet)
